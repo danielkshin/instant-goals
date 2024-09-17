2e9a25570e55e0e23b491889b933c7a5a0b916a7
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2024-09-16 20:18:46 -0700

    Remove test code and fix styling
    Whoops.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const day = date.getDate().toString().padStart(2, "0");
-    return `${year}${month}${day - 1}`;
+    return `${year}${month}${day}`;
   };
 
   useEffect(() => {
