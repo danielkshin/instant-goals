42b6a2a94cfd382273643045d159cda4e597254d
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2024-09-30 00:09:09 -0700

    Update footer with more info

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,34 @@
 export default function Footer() {
   return (
     <footer>
-      <p>created by</p>
-      <a href="https://danielkshin.github.io/" target="_blank" rel="noreferrer">
-        daniel shin
-      </a>
+      <p>
+        created by&nbsp;
+        <a
+          href="https://danielkshin.github.io/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          daniel shin
+        </a>
+        &nbsp;with&nbsp;
+        <a href="https://react.dev/" target="_blank" rel="noreferrer">
+          react.js
+        </a>
+        <br />
+        using&nbsp;
+        <a href="https://www.fotmob.com/" target="_blank" rel="noreferrer">
+          fotmob
+        </a>
+        &nbsp;&&nbsp;
+        <a
+          href="https://www.reddit.com/r/soccer/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          reddit
+        </a>
+        &nbsp;data
+      </p>
     </footer>
   );
 }
